7e2f605ad4ffa502aecc5269dfbc35fcccf40445
fabioono25 <fabioono25@gmail.com>
2023-07-21 21:09:14 +0200

    working with middlewares

---
+++ FirstApplication/MyMiddleware.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using Serilog;
+
+namespace AppTestOne
+{
+    public class MyMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            // do some action before
+
+            // call next middleware
+            await _next(httpContext);
+
+            // do something after
+        }
+    }
+
+    public class LogTimeMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LogTimeMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            // do some action before
+            var sw = Stopwatch.StartNew();
+
+            // call next middleware
+            await _next(httpContext);
+
+            // do something after
+            sw.Stop();
+
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            Log.Information($"The execution last {sw.Elapsed.TotalMilliseconds} ms ({sw.Elapsed.TotalSeconds} seconds)");   
+        }
+    }
+
+    public static class SerilogExtensions
+    {
+        public static void AddSerilog(this WebApplicationBuilder builder)
+        {
+            builder.Host.UseSerilog();
+        }
+    }
+
+    public static class LogTimeExtensions
+    {
+        public static void UseLogTime(this WebApplication app)
+        {
+            app.UseMiddleware<LogTimeMiddleware>();
+        }
+    }
+}
--- FirstApplication/Program.cs
+++ FirstApplication/Program.cs
@@ -1,22 +1,36 @@
 
 // Builder config
+using AppTestOne;
+using Serilog;
+
+// instance of .NET application
 var builder = WebApplication.CreateBuilder(args);
 
 
 // Pipeline config
 
 // Middleware config
+//builder.Host.UseSerilog();
+builder.AddSerilog(); // a better construction, using extension methods
 
 // Services config
-
+builder.Services.AddControllersWithViews(); // adding the ASP.NET MVC
 
 // App config
 var app = builder.Build();
 
+// behavior middleware
+//app.UseMiddleware<LogTimeMiddleware>();
+app.UseLogTime();
 
 // App's behaviors config
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/test", () =>
+{
+    Thread.Sleep(1000);
+    return "Test";
+});
 
 // Running the app
 app.Run();
