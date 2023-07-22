415a7f8f18c2056e042a9010dce2b4c274dc13a7
fabioono25 <fabioono25@gmail.com>
2023-07-22 11:49:22 +0200

    adding student

---
+++ DemoWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using DemoWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+
+        [HttpGet(Name = "students")]
+        public IEnumerable<Student> Get()
+        {
+            return new[] { new Student { Id=1, Name = "Jane"}, new Student { Id=2, Name = "John"} };
+        }
+    }
+}
