7c7bebf71a5d6aa88efb22ad0639c7f79bcca4e9
fabioono25 <fabioono25@gmail.com>
2023-08-13 00:10:56 +0200

    adding Nuget packages + first migration

--- ToDoGrpc/Program.cs
+++ ToDoGrpc/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoGrpc.Data;
 using ToDoGrpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite("Data Source=ToDoDB.db");
+});
 
 // Add services to the container.
 builder.Services.AddGrpc();
