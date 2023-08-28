74ce66acb81aa2920c0f95185c55456b88c28b8c
fabioono25 <fabioono25@gmail.com>
2023-08-28 10:38:00 +0200

    implementing UI

--- BasicStore.WebApp.MVC/Program.cs
+++ BasicStore.WebApp.MVC/Program.cs
@@ -51,7 +51,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Showcase}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
