28c9f0ed095a210ead331f5a9e12713cb22bbb4b
fabioono25 <fabioono25@gmail.com>
2023-08-28 10:05:19 +0200

    bootstrap UI + adding dependecies

---
+++ BasicStore.WebApp.MVC/Program.cs
@@ -0,0 +1,57 @@
+using BasicStore.WebApp.MVC.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using MediatR;
+using BasicStore.Catalog.Application.AutoMapper;
+using BasicStore.WebApp.MVC.Setup;
+using StoreDDD.Catalog.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDbContext<CatalogContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddAutoMapper(typeof(DomainToViewModelMappingProfile), typeof(ViewModelToDomainMappingProfile));
+builder.Services.AddMediatR(typeof(Program));
+
+builder.Services.RegisterServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
---
+++ BasicStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using BasicStore.Catalog.Application.Services;
+using BasicStore.Catalog.Data.Repositories;
+using BasicStore.Catalog.Domain.Events;
+using BasicStore.Catalog.Domain;
+using BasicStore.Core.Bus;
+using MediatR;
+using StoreDDD.Catalog.Data;
+
+namespace BasicStore.WebApp.MVC.Setup
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            // Domain Bus (Mediator)
+            services.AddScoped<IMediatrHandler, MediatrHandler>();
+
+            // Catalog
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductApplicationService, ProductApplicationService>();
+            services.AddScoped<IStockService, StockService>();
+            services.AddScoped<CatalogContext>();
+
+            services.AddScoped<INotificationHandler<ProductBellowStockEvent>, ProductEventHandler>();
+        }
+    }
+}
