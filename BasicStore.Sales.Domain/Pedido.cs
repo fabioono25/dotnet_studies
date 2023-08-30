45ba4616d4e47aa55c519eadf604d9f14b335272
fabioono25 <fabioono25@gmail.com>
2023-08-30 15:58:42 +0200

    bootstrapping Sales context

---
+++ BasicStore.Sales.Domain/Pedido.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicStore.Core.DomainObjects;
+
+namespace BasicStore.Sales.Domain
+{
+    public class Pedido: Entity, IAggregateRoot
+    {
+    }
+}
