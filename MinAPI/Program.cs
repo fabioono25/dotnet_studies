b495f80aaabd8dc18c17e202d80e9894d96c61d9
fabioono25 <fabioono25@gmail.com>
2023-12-11 21:30:44 +0100

    models, DTOs, migrations

--- MinAPI/Program.cs
+++ MinAPI/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MinAPI.data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var sqlConnBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("SqlDbConnection"));
+sqlConnBuilder.UserID = builder.Configuration["UserId"];
+sqlConnBuilder.Password = builder.Configuration["Password"];
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConnBuilder.ConnectionString));
 
 var app = builder.Build();
 
---
+++ MinAPI/data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinAPI.Models;
+
+namespace MinAPI.data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+            
+        }
+
+        public DbSet<Command> Commands => Set<Command>();
+    }
+}
