27ac7604a0dff1d1af9d431fe0f37c41b9083003
fabioono25 <fabioono25@gmail.com>
2023-12-13 09:44:21 +0100

    adding GET/POST endpoints

--- MinAPI/Program.cs
+++ MinAPI/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using MinAPI.data;
 using MinAPI.Dtos;
+using MinAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +16,7 @@ sqlConnBuilder.Password = builder.Configuration["Password"];
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConnBuilder.ConnectionString));
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetType());
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -33,6 +34,25 @@ app.MapGet("api/v1/commands", async (ICommandRepo repo, IMapper mapper) =>
     var commands = await repo.GetAllCommands();
 
     return Results.Ok(mapper.Map<IEnumerable<CommandReadDto>>(commands));
-});  
+});
+
+app.MapGet("api/v1/commands/{id}", async (ICommandRepo repo, IMapper mapper, int id) =>
+{
+    var command = await repo.GetCommandById(id);
+
+    return Results.Ok(mapper.Map<CommandReadDto>(command));
+});
+
+app.MapPost("api/v1/commands", async (ICommandRepo repo, IMapper mapper, CommandCreateDto cmdCreateDto) => {
+    var commandModel = mapper.Map<Command>(cmdCreateDto);
+
+    await repo.CreateCommand(commandModel);
+    await repo.SaveChanges();
+
+    var cmdReadDto = mapper.Map<CommandReadDto>(commandModel);
+
+    return Results.Created($"api/v1/commands/{cmdReadDto.Id}", cmdReadDto);
+
+});
 
 app.Run();
