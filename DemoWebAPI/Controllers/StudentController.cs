0dbe7866be40523c282c1ac8f6bb27386bdbddc2
fabioono25 <fabioono25@gmail.com>
2024-02-02 22:21:48 +0100

    working with routes

--- DemoWebAPI/Controllers/StudentController.cs
+++ DemoWebAPI/Controllers/StudentController.cs
@@ -13,5 +13,67 @@ namespace DemoWebAPI.Controllers
         {
             return new[] { new Student { Id = 1, Name = "Jane" }, new Student { Id = 2, Name = "John" } };
         }
+
+        //[HttpGet("{id}", Name = "student")]
+        //public Student Get(int id)
+        //{
+        //    return new Student { Id = id, Name = "Jane" };
+        //}
+
+        // another get, using ActionResult
+        // why ActionResult: because it can return NotFound() or Ok() or other status codes
+        [HttpGet("{id}", Name = "student")]
+        public ActionResult<Student> Get(int id)
+        {
+            if (id == 1)
+            {
+                return new Student { Id = id, Name = "Jane" };
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Student> Post(Student student)
+        {
+            if (student.Id == 1)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return CreatedAtRoute("student", new { id = student.Id }, student);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Student> Put(int id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok(student);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (id == 1)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
