2975470eb92737c1305ac9644d62787e5eb8728c
fabioono25 <fabioono25@gmail.com>
2024-10-21 19:13:15 +0200

    working with authentication and authorization

---
+++ BasicAuthentication/AuthenticationDemo/Authentication/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationDemo.Authentication;
+
+public class AppUser : IdentityUser
+{
+  // public string FirstName { get; set; }
+  // public string LastName { get; set; }
+  // public string ProfilePicture { get; set; }
+}
