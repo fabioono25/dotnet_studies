4b99befa3e97bb4beb9152b77e2db3c83871b0c5
fabioono25 <fabioono25@gmail.com>
2024-10-25 17:12:25 +0200

    authorization

---
+++ AuthorizationDemo/ClaimBasedAuthorizationDemo/end/ClaimBasedAuthorizationDemo/Authentication/AppClaimTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace ClaimBasedAuthorizationDemo.Authentication;
+
+public static class AppClaimTypes
+{
+    public const string DrivingLicenseNumber = "DrivingLicenseNumber";
+    public const string AccessNumber = "AccessNumber";
+
+}
